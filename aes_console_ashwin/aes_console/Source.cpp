a473cc653605c33b293ae5cf8a63556bcc7f0dbd
Ashwin Jose <aporuthu@asu.edu>
2018-11-24 17:30:14 -0700

    Added a temporary IV generation using time-stamp nonce.
    1. time-stamp nonce based IV generation.
    2. some code formatting

--- aes_console_ashwin/aes_console/Source.cpp
+++ aes_console_ashwin/aes_console/Source.cpp
@@ -3,7 +3,7 @@
 
 #include <iostream>
 #include <string>
-#include "math.h"
+#include <math.h>
 #include <sstream>
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,6 +11,7 @@
 #include <iomanip>
 #include <fstream>
 #include <vector>
+#include <chrono>
 
 using namespace std;
 
@@ -27,8 +28,9 @@ struct word
 };
 
 class AES {
-	// S-Box Initilalisation
-	byte sbox[256] = {
+
+	// S-Box Initialisation
+	const byte sbox[256] = {
 		byte{ 0x63 }, byte{ 0x7c }, byte{ 0x77 }, byte{ 0x7b },
 		byte{ 0xf2 }, byte{ 0x6b }, byte{ 0x6f }, byte{ 0xc5 },
 		byte{ 0x30 }, byte{ 0x01 }, byte{ 0x67 }, byte{ 0x2b },
@@ -95,8 +97,75 @@ class AES {
 		byte{ 0xb0 }, byte{ 0x54 }, byte{ 0xbb }, byte{ 0x16 }
 	};
 
-	byte inv_sbox[256] = { byte{ 0x52 }, byte{ 0x09 }, byte{ 0x6a }, byte{ 0xd5 }, byte{ 0x30 }, byte{ 0x36 }, byte{ 0xa5 }, byte{ 0x38 }, byte{ 0xbf }, byte{ 0x40 }, byte{ 0xa3 }, byte{ 0x9e }, byte{ 0x81 }, byte{ 0xf3 }, byte{ 0xd7 }, byte{ 0xfb },byte{ 0x7c }, byte{ 0xe3 }, byte{ 0x39 }, byte{ 0x82 }, byte{ 0x9b }, byte{ 0x2f }, byte{ 0xff }, byte{ 0x87 }, byte{ 0x34 }, byte{ 0x8e }, byte{ 0x43 }, byte{ 0x44 }, byte{ 0xc4 }, byte{ 0xde }, byte{ 0xe9 }, byte{ 0xcb },byte{ 0x54 }, byte{ 0x7b }, byte{ 0x94 }, byte{ 0x32 }, byte{ 0xa6 }, byte{ 0xc2 }, byte{ 0x23 }, byte{ 0x3d }, byte{ 0xee }, byte{ 0x4c }, byte{ 0x95 }, byte{ 0x0b }, byte{ 0x42 }, byte{ 0xfa }, byte{ 0xc3 }, byte{ 0x4e },byte{ 0x08 }, byte{ 0x2e }, byte{ 0xa1 }, byte{ 0x66 }, byte{ 0x28 }, byte{ 0xd9 }, byte{ 0x24 }, byte{ 0xb2 }, byte{ 0x76 }, byte{ 0x5b }, byte{ 0xa2 }, byte{ 0x49 }, byte{ 0x6d }, byte{ 0x8b }, byte{ 0xd1 }, byte{ 0x25 },byte{ 0x72 }, byte{ 0xf8 }, byte{ 0xf6 }, byte{ 0x64 }, byte{ 0x86 }, byte{ 0x68 }, byte{ 0x98 }, byte{ 0x16 }, byte{ 0xd4 }, byte{ 0xa4 }, byte{ 0x5c }, byte{ 0xcc }, byte{ 0x5d }, byte{ 0x65 }, byte{ 0xb6 }, byte{ 0x92 },byte{ 0x6c }, byte{ 0x70 }, byte{ 0x48 }, byte{ 0x50 }, byte{ 0xfd }, byte{ 0xed }, byte{ 0xb9 }, byte{ 0xda }, byte{ 0x5e }, byte{ 0x15 }, byte{ 0x46 }, byte{ 0x57 }, byte{ 0xa7 }, byte{ 0x8d }, byte{ 0x9d }, byte{ 0x84 },byte{ 0x90 }, byte{ 0xd8 }, byte{ 0xab }, byte{ 0x00 }, byte{ 0x8c }, byte{ 0xbc }, byte{ 0xd3 }, byte{ 0x0a }, byte{ 0xf7 }, byte{ 0xe4 }, byte{ 0x58 }, byte{ 0x05 }, byte{ 0xb8 }, byte{ 0xb3 }, byte{ 0x45 }, byte{ 0x06 },byte{ 0xd0 }, byte{ 0x2c }, byte{ 0x1e }, byte{ 0x8f }, byte{ 0xca }, byte{ 0x3f }, byte{ 0x0f }, byte{ 0x02 }, byte{ 0xc1 }, byte{ 0xaf }, byte{ 0xbd }, byte{ 0x03 }, byte{ 0x01 }, byte{ 0x13 }, byte{ 0x8a }, byte{ 0x6b },byte{ 0x3a }, byte{ 0x91 }, byte{ 0x11 }, byte{ 0x41 }, byte{ 0x4f }, byte{ 0x67 }, byte{ 0xdc }, byte{ 0xea }, byte{ 0x97 }, byte{ 0xf2 }, byte{ 0xcf }, byte{ 0xce }, byte{ 0xf0 }, byte{ 0xb4 }, byte{ 0xe6 }, byte{ 0x73 },byte{ 0x96 }, byte{ 0xac }, byte{ 0x74 }, byte{ 0x22 }, byte{ 0xe7 }, byte{ 0xad }, byte{ 0x35 }, byte{ 0x85 }, byte{ 0xe2 }, byte{ 0xf9 }, byte{ 0x37 }, byte{ 0xe8 }, byte{ 0x1c }, byte{ 0x75 }, byte{ 0xdf }, byte{ 0x6e },byte{ 0x47 }, byte{ 0xf1 }, byte{ 0x1a }, byte{ 0x71 }, byte{ 0x1d }, byte{ 0x29 }, byte{ 0xc5 }, byte{ 0x89 }, byte{ 0x6f }, byte{ 0xb7 }, byte{ 0x62 }, byte{ 0x0e }, byte{ 0xaa }, byte{ 0x18 }, byte{ 0xbe }, byte{ 0x1b },byte{ 0xfc }, byte{ 0x56 }, byte{ 0x3e }, byte{ 0x4b }, byte{ 0xc6 }, byte{ 0xd2 }, byte{ 0x79 }, byte{ 0x20 }, byte{ 0x9a }, byte{ 0xdb }, byte{ 0xc0 }, byte{ 0xfe }, byte{ 0x78 }, byte{ 0xcd }, byte{ 0x5a }, byte{ 0xf4 },byte{ 0x1f }, byte{ 0xdd }, byte{ 0xa8 }, byte{ 0x33 }, byte{ 0x88 }, byte{ 0x07 }, byte{ 0xc7 }, byte{ 0x31 }, byte{ 0xb1 }, byte{ 0x12 }, byte{ 0x10 }, byte{ 0x59 }, byte{ 0x27 }, byte{ 0x80 }, byte{ 0xec }, byte{ 0x5f },byte{ 0x60 }, byte{ 0x51 }, byte{ 0x7f }, byte{ 0xa9 }, byte{ 0x19 }, byte{ 0xb5 }, byte{ 0x4a }, byte{ 0x0d }, byte{ 0x2d }, byte{ 0xe5 }, byte{ 0x7a }, byte{ 0x9f }, byte{ 0x93 }, byte{ 0xc9 }, byte{ 0x9c }, byte{ 0xef },byte{ 0xa0 }, byte{ 0xe0 }, byte{ 0x3b }, byte{ 0x4d }, byte{ 0xae }, byte{ 0x2a }, byte{ 0xf5 }, byte{ 0xb0 }, byte{ 0xc8 }, byte{ 0xeb }, byte{ 0xbb }, byte{ 0x3c }, byte{ 0x83 }, byte{ 0x53 }, byte{ 0x99 }, byte{ 0x61 },byte{ 0x17 }, byte{ 0x2b }, byte{ 0x04 }, byte{ 0x7e }, byte{ 0xba }, byte{ 0x77 }, byte{ 0xd6 }, byte{ 0x26 }, byte{ 0xe1 }, byte{ 0x69 }, byte{ 0x14 }, byte{ 0x63 }, byte{ 0x55 }, byte{ 0x21 }, byte{ 0x0c }, byte{ 0x7d } };
+	// Inverse S-Box Initialisation
+	const byte inv_sbox[256] = { 
+		byte{ 0x52 }, byte{ 0x09 }, byte{ 0x6a }, byte{ 0xd5 },
+		byte{ 0x30 }, byte{ 0x36 }, byte{ 0xa5 }, byte{ 0x38 },
+		byte{ 0xbf }, byte{ 0x40 }, byte{ 0xa3 }, byte{ 0x9e },
+		byte{ 0x81 }, byte{ 0xf3 }, byte{ 0xd7 }, byte{ 0xfb },
+		byte{ 0x7c }, byte{ 0xe3 }, byte{ 0x39 }, byte{ 0x82 },
+		byte{ 0x9b }, byte{ 0x2f }, byte{ 0xff }, byte{ 0x87 },
+		byte{ 0x34 }, byte{ 0x8e }, byte{ 0x43 }, byte{ 0x44 },
+		byte{ 0xc4 }, byte{ 0xde }, byte{ 0xe9 }, byte{ 0xcb },
+		byte{ 0x54 }, byte{ 0x7b }, byte{ 0x94 }, byte{ 0x32 },
+		byte{ 0xa6 }, byte{ 0xc2 }, byte{ 0x23 }, byte{ 0x3d },
+		byte{ 0xee }, byte{ 0x4c }, byte{ 0x95 }, byte{ 0x0b },
+		byte{ 0x42 }, byte{ 0xfa }, byte{ 0xc3 }, byte{ 0x4e },
+		byte{ 0x08 }, byte{ 0x2e }, byte{ 0xa1 }, byte{ 0x66 },
+		byte{ 0x28 }, byte{ 0xd9 }, byte{ 0x24 }, byte{ 0xb2 },
+		byte{ 0x76 }, byte{ 0x5b }, byte{ 0xa2 }, byte{ 0x49 },
+		byte{ 0x6d }, byte{ 0x8b }, byte{ 0xd1 }, byte{ 0x25 },
+		byte{ 0x72 }, byte{ 0xf8 }, byte{ 0xf6 }, byte{ 0x64 },
+		byte{ 0x86 }, byte{ 0x68 }, byte{ 0x98 }, byte{ 0x16 },
+		byte{ 0xd4 }, byte{ 0xa4 }, byte{ 0x5c }, byte{ 0xcc },
+		byte{ 0x5d }, byte{ 0x65 }, byte{ 0xb6 }, byte{ 0x92 },
+		byte{ 0x6c }, byte{ 0x70 }, byte{ 0x48 }, byte{ 0x50 },
+		byte{ 0xfd }, byte{ 0xed }, byte{ 0xb9 }, byte{ 0xda },
+		byte{ 0x5e }, byte{ 0x15 }, byte{ 0x46 }, byte{ 0x57 },
+		byte{ 0xa7 }, byte{ 0x8d }, byte{ 0x9d }, byte{ 0x84 },
+		byte{ 0x90 }, byte{ 0xd8 }, byte{ 0xab }, byte{ 0x00 },
+		byte{ 0x8c }, byte{ 0xbc }, byte{ 0xd3 }, byte{ 0x0a },
+		byte{ 0xf7 }, byte{ 0xe4 }, byte{ 0x58 }, byte{ 0x05 },
+		byte{ 0xb8 }, byte{ 0xb3 }, byte{ 0x45 }, byte{ 0x06 },
+		byte{ 0xd0 }, byte{ 0x2c }, byte{ 0x1e }, byte{ 0x8f },
+		byte{ 0xca }, byte{ 0x3f }, byte{ 0x0f }, byte{ 0x02 },
+		byte{ 0xc1 }, byte{ 0xaf }, byte{ 0xbd }, byte{ 0x03 },
+		byte{ 0x01 }, byte{ 0x13 }, byte{ 0x8a }, byte{ 0x6b },
+		byte{ 0x3a }, byte{ 0x91 }, byte{ 0x11 }, byte{ 0x41 },
+		byte{ 0x4f }, byte{ 0x67 }, byte{ 0xdc }, byte{ 0xea },
+		byte{ 0x97 }, byte{ 0xf2 }, byte{ 0xcf }, byte{ 0xce },
+		byte{ 0xf0 }, byte{ 0xb4 }, byte{ 0xe6 }, byte{ 0x73 },
+		byte{ 0x96 }, byte{ 0xac }, byte{ 0x74 }, byte{ 0x22 },
+		byte{ 0xe7 }, byte{ 0xad }, byte{ 0x35 }, byte{ 0x85 },
+		byte{ 0xe2 }, byte{ 0xf9 }, byte{ 0x37 }, byte{ 0xe8 },
+		byte{ 0x1c }, byte{ 0x75 }, byte{ 0xdf }, byte{ 0x6e },
+		byte{ 0x47 }, byte{ 0xf1 }, byte{ 0x1a }, byte{ 0x71 },
+		byte{ 0x1d }, byte{ 0x29 }, byte{ 0xc5 }, byte{ 0x89 },
+		byte{ 0x6f }, byte{ 0xb7 }, byte{ 0x62 }, byte{ 0x0e },
+		byte{ 0xaa }, byte{ 0x18 }, byte{ 0xbe }, byte{ 0x1b },
+		byte{ 0xfc }, byte{ 0x56 }, byte{ 0x3e }, byte{ 0x4b },
+		byte{ 0xc6 }, byte{ 0xd2 }, byte{ 0x79 }, byte{ 0x20 },
+		byte{ 0x9a }, byte{ 0xdb }, byte{ 0xc0 }, byte{ 0xfe },
+		byte{ 0x78 }, byte{ 0xcd }, byte{ 0x5a }, byte{ 0xf4 },
+		byte{ 0x1f }, byte{ 0xdd }, byte{ 0xa8 }, byte{ 0x33 },
+		byte{ 0x88 }, byte{ 0x07 }, byte{ 0xc7 }, byte{ 0x31 },
+		byte{ 0xb1 }, byte{ 0x12 }, byte{ 0x10 }, byte{ 0x59 },
+		byte{ 0x27 }, byte{ 0x80 }, byte{ 0xec }, byte{ 0x5f },
+		byte{ 0x60 }, byte{ 0x51 }, byte{ 0x7f }, byte{ 0xa9 },
+		byte{ 0x19 }, byte{ 0xb5 }, byte{ 0x4a }, byte{ 0x0d },
+		byte{ 0x2d }, byte{ 0xe5 }, byte{ 0x7a }, byte{ 0x9f },
+		byte{ 0x93 }, byte{ 0xc9 }, byte{ 0x9c }, byte{ 0xef },
+		byte{ 0xa0 }, byte{ 0xe0 }, byte{ 0x3b }, byte{ 0x4d },
+		byte{ 0xae }, byte{ 0x2a }, byte{ 0xf5 }, byte{ 0xb0 },
+		byte{ 0xc8 }, byte{ 0xeb }, byte{ 0xbb }, byte{ 0x3c },
+		byte{ 0x83 }, byte{ 0x53 }, byte{ 0x99 }, byte{ 0x61 },
+		byte{ 0x17 }, byte{ 0x2b }, byte{ 0x04 }, byte{ 0x7e },
+		byte{ 0xba }, byte{ 0x77 }, byte{ 0xd6 }, byte{ 0x26 },
+		byte{ 0xe1 }, byte{ 0x69 }, byte{ 0x14 }, byte{ 0x63 },
+		byte{ 0x55 }, byte{ 0x21 }, byte{ 0x0c }, byte{ 0x7d }
+	};
 
+	state IV;
 
 public:
 
@@ -107,19 +176,21 @@ public:
 		vector<struct state*> output;
 
 		output.push_back(new state);
-		//TODO
 		//GENERATE IV RANDOMLY AND SET AS THE FIRST OUTPUT BLOCK
-		//HARDCODED FOR NOW
-		for (int i = 0; i < 4; i++)
-		{
-			for (int j = 0; j < 4; j++)
-			{
-				output.back()->stateByte[i][j] = byte{ 0x01 };
-			}
-		}
+		generateIV();
+		*(output.back()) = blockEncrypt(IV, key);
+
+		////HARDCODED FOR NOW
+		//for (int i = 0; i < 4; i++)
+		//{
+		//	for (int j = 0; j < 4; j++)
+		//	{
+		//		output.back()->stateByte[i][j] = byte{ 0x01 };
+		//	}
+		//}
 		
 		//for each input block, create an encrypted output block in CBC mode
-		for (int i = 0; i < input.size(); i++)
+		for (unsigned int i = 0; i < input.size(); i++)
 		{
 			output.push_back(new state);
 			*(output.back()) = blockEncrypt(xorBlock(output[i],input[i]), key);
@@ -134,7 +205,7 @@ public:
 		vector<struct state*> output;
 		//for each input block, xor with the previous block
 		//IV is the first block, so start from i=1
-		for (int i = 1; i < input.size(); i++)
+		for (unsigned int i = 1; i < input.size(); i++)
 		{
 			output.push_back(new state);
 			*(output.back()) = xorBlock(&blockDecrypt(*input[i], key), input[i-1]);
@@ -143,6 +214,27 @@ public:
 	}
 
 private:
+
+	// Random IV Generator using timestamp nonce
+	void generateIV()
+	{
+		using namespace std::chrono;
+		nanoseconds ns = duration_cast<nanoseconds>(system_clock::now().time_since_epoch());
+		std::stringstream stream;
+		long long int value = ns.count();
+		int tempWord;
+		for (int i = 0; i < 4; i++)
+		{
+			for (int j = 0; j < 4; j++)
+			{
+				tempWord = value % 10;
+				stream << std::hex << tempWord;
+				IV.stateByte[i][j] = std::byte(tempWord);
+				value /= 10;
+			}
+		}
+	}
+
 	//S-Box substitution
 	byte SubBytes(byte a)
 	{
@@ -261,10 +353,10 @@ private:
 	}
 
 	//used in MixColumns and inverse MixColumns to multiply a byte with one of the constants
-	byte mixMultiply(byte variable, byte constant)
+	byte mixMultiply(byte variable, byte constantByte)
 	{
 		//check which constant is being multiplied
-		if (constant == byte{ 0x02 })
+		if (constantByte == byte{ 0x02 })
 		{
 			//if it is 0x02, check the highest order bit for overflow
 			if ((variable >> 7) == byte{ 0x01 })
@@ -272,27 +364,27 @@ private:
 			else
 				return ((variable << 1) ^ byte { 0x00 });
 		}
-		else if (constant == byte{ 0x03 })
+		else if (constantByte == byte{ 0x03 })
 		{
 			//in the case of 0x03 we call again for 0x02 and the variable value, before XORing with its original value
 			return mixMultiply(variable, byte{ 0x02 }) ^ variable;
 		}
-		else if (constant == byte{ 0x09 })
+		else if (constantByte == byte{ 0x09 })
 		{
 			//in the case of 0x09, we turn it into 0x08 + 0x01
 			return mixMultiply(mixMultiply(mixMultiply(variable, byte{ 0x02 }), byte{ 0x02 }), byte{ 0x02 }) ^ variable;
 		}
-		else if (constant == byte{ 0x0b })
+		else if (constantByte == byte{ 0x0b })
 		{
 			//in the case of 0x0b, we turn it into 0x08 + 0x02 + 0x01
 			return mixMultiply(mixMultiply(mixMultiply(variable, byte{ 0x02 }), byte{ 0x02 }), byte{ 0x02 }) ^ mixMultiply(variable, byte{ 0x02 }) ^ variable;
 		}
-		else if (constant == byte{ 0x0d })
+		else if (constantByte == byte{ 0x0d })
 		{
 			//in the case of 0x0d, we turn it into 0x08 + 0x04 + 0x01
 			return mixMultiply(mixMultiply(mixMultiply(variable, byte{ 0x02 }), byte{ 0x02 }), byte{ 0x02 }) ^ mixMultiply(mixMultiply(variable, byte{ 0x02 }), byte{ 0x02 }) ^ variable;
 		}
-		else if (constant == byte{ 0x0e })
+		else if (constantByte == byte{ 0x0e })
 		{
 			//in the case of 0x0e, we turn it into 0x08 + 0x04 + 0x02
 			return mixMultiply(mixMultiply(mixMultiply(variable, byte{ 0x02 }), byte{ 0x02 }), byte{ 0x02 }) ^ mixMultiply(mixMultiply(variable, byte{ 0x02 }), byte{ 0x02 }) ^ mixMultiply(variable, byte{ 0x02 });
@@ -618,7 +710,7 @@ int main()
 	
 	//create an input and output vector of struct states, to contain blocks of input or output
 	vector<struct state*> input;
-	struct state test;
+	//struct state test;
 	//vector<struct state*> output;
 	//TODO
 
